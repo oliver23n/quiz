0ab9c549764b8136b687a0bc29e565ecf3c89646
oliver23n <oliver.nasevski@gmail.com>
2023-04-19 21:58:57 -0400

    started js code

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -13,4 +13,35 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+const quizTitle = document.getElementById('title');
+const desc = document.querySelector("p");
+const startButton = document.getElementById("start");
+const timer = document.getElementById("time");
+//when you click start the quiz starts
+function startQuiz (){
+    let time = 60;
+    removeStart();
+    //timer starts
+    //show timer in display
+    displayTimer(time);
+    // const timerInterval = setInterval( function (){
 
+    // },1000 );
+    //quiz title,start button, description gets removed
+    //question gets created
+    //answer buttons are created
+
+
+}
+//removes quiz title, description and start button
+function removeStart(){
+    quizTitle.remove();
+    desc.remove();
+    startButton.remove();
+}
+//shows timer 
+function displayTimer(num){
+    timer.textContent = "Time: "+ num;
+}
+
+startButton.addEventListener("click",startQuiz);
